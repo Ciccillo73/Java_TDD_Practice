bd963118e79ae928568a4b03716de81a325ae0b0
Francesco <med.dama@gmail.com>
2020-05-25 18:13:34 +0100

    adds new class and test the has sheets number

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,16 @@
 public class Printer {
+
+    private int sheetsLeft;
+    private int toner;
+
+    public Printer(int sheetsLeft, int toner) {
+        this.sheetsLeft = sheetsLeft;
+        this.toner = toner;
+    }
+    public int getSheetsLeft(){
+        return this.sheetsLeft;
+    }
+
+
+
 }
--- src/test/java/TestPrinter.java
+++ src/test/java/TestPrinter.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestPrinter {
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(100, 4);
+    }
+
+    @Test
+    public void hasNumberOfSheets(){
+        assertEquals(100, printer.getSheetsLeft());
+    }
 }
